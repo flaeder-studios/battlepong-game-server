73d2147d5bf945377ab137200710b19402b4c8d3
daniel-skog <jda.skog@gmail.com>
2015-11-02 21:45:58 +0100

    Bouncing balls!

--- static/mpong/js/mpong.js
+++ static/mpong/js/mpong.js
@@ -1,35 +1,37 @@
 var BALL_EDGES = 32
 var D_ANGLE = 2*Math.PI/BALL_EDGES
 
-function main() {
-  // Get A WebGL context
-  var canvas = document.getElementById("canvas");
-  var gl = getWebGLContext(canvas);
-  if (!gl) {
-    return;
+var xspd = 0.1;
+var yspd = -0.5;
+var xpos = 0.0
+var ypos = 0.0
+var radius = 0.05
+var dt = 0.05
+var gl
+
+function createCircle(radius, xpos, ypos) {
+  var angle = 0.0;
+  arr = [xpos, ypos];
+  for(var i = 2; i < 2*(BALL_EDGES + 1) + 2; i += 2) {
+    arr[i] = radius*Math.cos(angle) + xpos;
+    arr[i+1] = radius*Math.sin(angle) + ypos;
+    angle += D_ANGLE;
   }
+  return arr;
+}
+
+function drawCircle(radius, xpos, ypos) {
 
   // setup GLSL program
   var program = createProgramFromScripts(gl, ["2d-vertex-shader", "2d-fragment-shader"]);
   gl.useProgram(program);
 
   // Create a circle 
-  var angle = 0.0
-  var arr = 
-  arr = [0.0, 0.0]
-  for(var i = 2; i < 2*(BALL_EDGES + 1) + 2; i += 2) {
-    arr[i] = Math.cos(angle)
-    arr[i+1] = Math.sin(angle)
-    angle += D_ANGLE
-  }
+  arr = createCircle(radius, xpos, ypos);
 
   // look up where the vertex data needs to go.
   var positionLocation = gl.getAttribLocation(program, "a_position");
 
-  gl.clearColor(0.0, 0.0, 0.0, 1.0);
-
-  // Create a buffer and put a single clipspace rectangle in
-  // it (2 triangles)
   var buffer = gl.createBuffer();
   gl.bindBuffer(gl.ARRAY_BUFFER, buffer);
   gl.bufferData(gl.ARRAY_BUFFER, new Float32Array(arr), gl.STATIC_DRAW);
@@ -39,20 +41,45 @@ function main() {
   // draw
   gl.drawArrays(gl.TRIANGLE_FAN, 0, arr.length/2);
 
-  arr = [
-        -1.0, -1.0,
-        1.0, -1.0,
-        -1.0,  1.0,
-        -1.0,  1.0,
-        1.0, -1.0,
-        1.0,  1.0];
+}
 
-  gl.bufferData(
-    gl.ARRAY_BUFFER,
-    new Float32Array(arr),
-    gl.STATIC_DRAW);
+function handleCollision() {
+    if(xpos + radius > 1) {
+        xspd = -xspd
+        xpos = 0.9999 - radius
+    } else if(xpos - radius < -1) {
+        xspd = -xspd
+        xpos = -0.9999 + radius
+    } else if(ypos + radius > 1) {
+        yspd = -yspd
+        ypos = 0.9999 - radius
+    } else if(ypos - radius < -1) {
+        yspd = -yspd
+        ypos = -0.9999 + radius
+    } else {
+        // No bounce...
+    }
+}
 
-    //gl.drawArrays(gl.TRIANGLE_FAN, 0, 6);
+function moveCircle() {
+    handleCollision()
+    xpos += xspd*dt
+    ypos += yspd*dt
+}
+
+function action() {
+  moveCircle()
+  drawCircle(radius, xpos, ypos)
+}
+
+function main() {
+  // Get A WebGL context
+  var canvas = document.getElementById("canvas");
+  gl = getWebGLContext(canvas);
+  if (!gl) {
+    return;
+  }
+  setInterval(action, dt*1000)
 }
 
 "use strict";
