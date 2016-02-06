0adb48b6210815fde0bef2e2fb1643d61eab419f
Olof <svedvall@gmail.com>
2016-02-06 20:41:29 +0100

    Changed PlayerHandler to only add a player name.

--- handlers/playerHandler.py
+++ handlers/playerHandler.py
@@ -16,9 +16,6 @@ class PlayerHandler:
         else:
             raise cherrypy.HTTPError(401, 'player name not set')
         
-        player['currentGame'] = cherrypy.session['currentGame']
-        player['createdGames'] = cherrypy.session['createdGames']
-
         cherrypy.log(str(player))
         
         return { 'player': player }
@@ -27,19 +24,22 @@ class PlayerHandler:
     @cherrypy.tools.json_out()
     @cherrypy.tools.json_in()
     def POST(self):
-        data = cherrypy.request.json
-        
-        if 'player' in data:
-            data = data['player']
-        
-        if 'name' in data:
-            cherrypy.session['name'] = str(data['name'])
-        else:
-            raise cherrypy.HTTPError(401, 'player name not set')
-            
-        cherrypy.session['currentGame'] = []
-        cherrypy.session['createdGames'] = []
+        """ This method creates a player. A player name must be unique.'
+        only one game can be created per name."""
 
-        cherrypy.log("set name to %s" % data['name'])
+        data = cherrypy.request.json
+
+        if 'player' in data and 'name' in data['player']:
+            playerName = str(data['player']['name'])
+        else:
+            raise cherrypy.HTTPError(401, 'player name not found')
+
+        for g in GameHandler.games:
+            if g['createdBy'] == playerName:
+                raise cherrypy.HTTPError(401, 'player name not set')
+        
+        cherrypy.session['name'] = playerName
+
+        cherrypy.log("set name to %s" % (playerName))
         
         return self.GET()
