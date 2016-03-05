3d789b06ef5d97c0b044a302d1aec744ead5af69
Olof <svedvall@gmail.com>
2016-03-05 18:58:06 +0100

    Added exceptions.

--- mpong/masterGameBuilder.py
+++ mpong/masterGameBuilder.py
@@ -1,4 +1,4 @@
-import model
+import mpong.model
 
 
 class MasterGameBuilder(object):
@@ -8,37 +8,52 @@ class MasterGameBuilder(object):
     def createPlayer(self, name):
         if name not in players.keys():
             players[name] = model.Player(name)
+        else:
+            raise cherrypy.HTTPError(400, 'Player %s already exists' % name)
 
-    def createGame(self, gameID, creatorName, maxPlayers):
-        if gameID not in games.keys():
-            games[gameID] = model.MpongGame(gameID, maxPlayers)
-            self.join(gameID, creatorName)
+    def createGame(self, gameId, creatorName, maxPlayers):
+        if gameId not in games.keys():
+            games[gameId] = model.MPongGame(gameId, maxPlayers)
+            self.join(gameId, creatorName)
+        else:
+            raise cherrypy.HTTPError(400, 'Game already %d exists' % gameId)
 
-    def join(self, gameID, name):
-        if name in players.keys() and gameID in games.keys():
-            games[gameID].joinPlayer(players[name])
+    def join(self, gameId, name):
+        if name in players.keys() and gameId in games.keys():
+            games[gameId].joinPlayer(players[name])
+        else:
+            raise cherrypy.HTTPError(400, 'No game with id %s found' % gameId)
 
-    def leave(self, gameID, name):
-        if name in players.keys() and gameID in games.keys():
-            games[gameID].leavePlayer(player[name])
+    def leave(self, gameId, name):
+        if name in players.keys() and gameId in games.keys():
+            games[gameId].leavePlayer(player[name])
+        else:
+            raise cherrypy.HTTPError(400, 'No game with id %s found' % gameId)
 
-    def startGame(self, gameID):
-        if gameID in games.keys():
-            games[gameID].start()
+    def startGame(self, gameId):
+        if gameId in games.keys() and games[gameId].maxPlayers == len(games[gameId].joinedPlayers):
+            games[gameId].start()
+        else:
+            raise cherrypy.HTTPError(400, 'No game with id %s found or not enough players, who knows.' % gameId)
 
-    def stopGame(self, gameID):
-        if gameID in games.keys():
-            games[gameID].stop()
-            del games[gameID]
+    def stopGame(self, gameId):
+        if gameId in games.keys():
+            games[gameId].stop()
+            del games[gameId]
+        else:
+            raise cherrypy.HTTPError(400, 'No game with id %s found' % gameId)
 
     def setPlayerSpeed(self, playerName, speedY):
         """ Set player speed in y-direction."""
-        players[playerName].velocity = model.Vector(0, float(speedY))
-
-    def gameState(self, gameID):
-        if gameID in games.keys():
-            return games[gameID].getState()
+        if playerName in players.keys():
+            players[playerName].velocity = model.Vector(0, float(speedY))
         else:
-            return None
+            raise cherrypy.HTTPError(400, 'No player %s found' % playerName)
+
+    def gameState(self, gameId):
+        if gameId in games.keys():
+            return games[gameId].getState()
+        else:
+            raise cherrypy.HTTPError(404, 'No game with id %s found' % gameId)
 
 masterGame = MasterGameBuilder()
