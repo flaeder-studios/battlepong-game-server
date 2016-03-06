089c1685e3a019cd97d3b942961b38cc53204571
daniel-skog <jda.skog@gmail.com>
2016-03-06 11:07:31 +0100

    minor fixes to auto-resize

--- static/app/shared/canvas/autoresize.directive.js
+++ static/app/shared/canvas/autoresize.directive.js
@@ -8,16 +8,19 @@ angular.module("flaederGamesApp")
                 scope.ratio = parseInt(attrs.width) / parseInt(attrs.height);
 
                 function resize() {
-                    var footer = angular.element(document).find('footer'),
-                        header = angular.element(document).find('header'),
-                        gameControlBar = document.getElementById('gameControlBar'),
-                        hsize = $window.innerHeight - footer.prop('offsetHeight') - header.prop('offsetHeight') - gameControlBar.offsetHeight - 80,
-                        wsize = hsize * scope.ratio;
-                    element.css('height', hsize.toString() + 'px');
-                    element.css('width', wsize.toString() + 'px');
+                    if (!document.fullscreenElement &&    // alternative standard method
+                      !document.mozFullScreenElement && !document.webkitFullscreenElement && !document.msFullscreenElement ) {  // current working methods
+
+                        var footer = angular.element(document).find('footer'),
+                            header = angular.element(document).find('header'),
+                            gameControlBar = document.getElementById('gameControlBar'),
+                            hsize = $window.innerHeight - footer.prop('offsetHeight') - header.prop('offsetHeight') - gameControlBar.offsetHeight - 80,
+                            wsize = hsize * scope.ratio;
+                        element.css('height', hsize.toString() + 'px');
+                        element.css('width', wsize.toString() + 'px');
+                    }
                 }
 
-                console.log("attrs:", attrs);
                 $window.onresize = resize;
                 resize();
 
