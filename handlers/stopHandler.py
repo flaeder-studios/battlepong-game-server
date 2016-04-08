b4bab05136ff96bf4ee8c55915009173a0d43371
Olof <svedvall@gmail.com>
2016-04-08 22:04:59 +0200

    modified:   handlers/stopHandler.py Added leave game when stoped.

--- handlers/stopHandler.py
+++ handlers/stopHandler.py
@@ -15,5 +15,8 @@ class StopHandler:
         playerName = cherrypy.session.get('name')
         currentGame = masterGame.getCurrentGame(playerName)
         masterGame.stopGame(currentGame['id'])
-        cherrypy.log('StopHandler: Stop game %s' % currentGame)
-        return {'game':[currentGame]}
+        masterGame.leave(currentGame['id'], playerName)
+
+        game = masterGame.getGameData(currentGame['id'])
+        cherrypy.log('StopHandler: stop and leave game %s' % game)
+        return {'game':[game]}
