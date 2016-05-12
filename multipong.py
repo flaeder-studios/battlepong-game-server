548071ea5a6b419258a921de3200d46391922dc4
Olof <svedvall@gmail.com>
2016-05-12 14:27:45 +0200

    modified:   multipong.py 	new file:   multipongApplication.py Created a file containing only the application. Server start from multipong.py as usual.

--- multipong.py
+++ multipong.py
@@ -4,37 +4,16 @@
 import os
 import cherrypy
 import json
-import handlers
-import mpong.controlUnit
+from multipongApplication import root
 from ws4py.websocket import EchoWebSocket
 
+
 if __name__ == "__main__":
-    def handleError():
-        cherrypy.response.status = 500
-        cherrypy.response.body = ["An error occurred..."]
-    
     def standardErrorMessage(status, message, traceback, version):
         response = cherrypy.response
         response.headers['Content-Type'] = 'application/json'
         return json.dumps({'status': status, 'message': message, 'traceback': traceback, 'version': version})
-    
-    
-    class Root:
-        _cp_config = {'request.error_response': handleError}
-    
-    
-    root = Root()
-    root.game = handlers.GameHandler()
-    root.game.method = Root()
-    root.game.method.join = handlers.JoinGameHandler()
-    root.game.method.leave = handlers.LeaveGameHandler()
-    root.game.method.start = handlers.StartHandler()
-    root.game.method.quit = handlers.StopHandler()
-    root.game.state = handlers.GameState()
-    root.game.paddle = handlers.PaddleHandler()
-    root.player = handlers.PlayerHandler()
-    root.ws = handlers.WebSocketHandler()
-    
+
     cherrypy.config.update({'error_page.default': standardErrorMessage})
     cherrypy.config.update({'log.screen': True,
                             'log.access_file': '',
@@ -42,8 +21,5 @@ if __name__ == "__main__":
                             'server.thread_pool': 30,
                             'server.socket_host': "0.0.0.0",
                             'server.socket_port': 8080})
-    
-    
-    
     cfgFile = os.path.dirname(os.path.realpath(__file__)) + '/multipong.conf'
     cherrypy.quickstart(root, '/', cfgFile)
---
+++ multipongApplication.py
@@ -0,0 +1,24 @@
+
+import handlers
+
+
+def handleError():
+    cherrypy.response.status = 500
+    cherrypy.response.body = ["An error occurred..."]
+
+
+class Root:
+    _cp_config = {'request.error_response': handleError}
+
+
+root = Root()
+root.game = handlers.GameHandler()
+root.game.method = Root()
+root.game.method.join = handlers.JoinGameHandler()
+root.game.method.leave = handlers.LeaveGameHandler()
+root.game.method.start = handlers.StartHandler()
+root.game.method.quit = handlers.StopHandler()
+root.game.state = handlers.GameState()
+root.game.paddle = handlers.PaddleHandler()
+root.player = handlers.PlayerHandler()
+root.ws = handlers.WebSocketHandler()
